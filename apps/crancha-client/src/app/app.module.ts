a012aa89deb2054c2cdad8d853f25c294f4734da
Mihail Madzharov <mihail.madzharov@falcon.io>
2020-05-14 20:15:07 +0300

    feat(Ionic): Add Ionic componetns and side menu to project

--- apps/crancha-client/src/app/app.module.ts
+++ apps/crancha-client/src/app/app.module.ts
@@ -5,9 +5,16 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShellModule } from '@crancha/public';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicModule } from '@ionic/angular';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, ShellModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ShellModule,
+    AppRoutingModule,
+    IonicModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- libs/shell/src/lib/shell.module.ts
+++ libs/shell/src/lib/shell.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShellComponent } from './shell.component';
 import { ShellRoutingModule } from './shell-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  imports: [CommonModule, ShellRoutingModule],
+  imports: [CommonModule, ShellRoutingModule, IonicModule],
   declarations: [ShellComponent]
 })
 export class ShellModule {}
