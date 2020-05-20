dda402c418ab054c2fb63f3957e479b5b3f6a78d
Mihail Madzharov <mihail.madzharov@falcon.io>
2020-05-20 19:53:58 +0300

    add change of map type id

--- libs/feature-map/src/lib/map.component.ts
+++ libs/feature-map/src/lib/map.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MapFacade } from '@crancha/domain';
-
+const mapTerrain = [
+  'hybrid',
+  /** This map type displays a normal street map. */
+  'roadmap',
+  /** This map type displays satellite images. */
+  'satellite',
+  /** This map type displays maps with physical features such as terrain and vegetation. */
+  'terrain'
+];
 @Component({
   selector: 'crancha-map',
   templateUrl: './map.component.html',
@@ -10,6 +18,7 @@ export class MapComponent implements OnInit {
   public paths = this.mapFacade.paths;
   constructor(private mapFacade: MapFacade) {}
   public center = { lat: 42.0987626, lng: 24.1593068 };
+  public mapTerrain = mapTerrain;
   ngOnInit() {
     navigator.geolocation.getCurrentPosition(
       position => {
